f5723d1db03ecc3fd64a9f9e4d2b7813febf0564
sammun1926 <munorusam10@gmail.com>
2023-03-03 03:23:33 +0300

    initialize vue project

--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//import bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import {BootstrapVue,IconsPluing} from "bootstrap-vue";
+//make bootsrap globally available for use
+Vue.use(BootstrapVue);
+Vue.use(IconsPluging);
+
+import vuetify from './plugins/vuetify'
+
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
